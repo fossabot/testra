b333fd721ec2168ef70e8ed6b66b299f1589d8bd
testra <testra2018@gmail.com>
2018-06-23 12:04:35 +0100

    Generated using ngx-rocket and added counters component

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CoreModule } from '@app/core';
+import { SharedModule } from '@app/shared';
+import { HomeModule } from './home/home.module';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CountersComponent } from './counters/counters.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot(),
+    NgbModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    AppRoutingModule
+  ],
+  declarations: [AppComponent, CountersComponent],
+  providers: [
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
